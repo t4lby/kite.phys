b75468e792c5cac3336a1df8637575c0e6695f8f
t4lby <alextalbot2@hotmail.co.uk>
2020-08-10 16:23:23 +0100

    init

---
+++ Assets/Kiter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kiter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody>().AddForce(0.2f, 0f, 0.1f);
+	}
+}
